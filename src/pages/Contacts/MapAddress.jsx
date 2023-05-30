8c261c5c65d69e2e265ca05857e47ed519baa357
João Lázaro <joao.lazaro@outlook.com>
2023-05-30 16:38:23 +0100

    finished contact page map

--- src/pages/Contacts/MapAddress.jsx
+++ src/pages/Contacts/MapAddress.jsx
@@ -1,8 +1,27 @@
+import { useMemo } from 'react';
+import { GoogleMap, MarkerF, useLoadScript } from '@react-google-maps/api';
+import LoaderCircle from '../../features/UI/LoaderCircle';
+
 export default function MapAddress() {
+	const { isLoaded } = useLoadScript({
+		googleMapsApiKey: import.meta.env.VITE_GMAPS_APIKEY,
+	});
+	const centered = useMemo(
+		() => ({ lat: 38.72545717441236, lng: -9.149991302840245 }),
+		[]
+	);
+
+	if (!isLoaded) return <LoaderCircle />;
+
 	return (
 		<article className='map-address'>
-			<header></header>
-			wrapper
+			<GoogleMap
+				zoom={16}
+				center={centered}
+				mapContainerClassName='map-container'
+			>
+				<MarkerF position={centered} />
+			</GoogleMap>
 		</article>
 	);
 }
